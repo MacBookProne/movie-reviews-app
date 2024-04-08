45aa01b21a6c64c02dd18d2d515a6b48ce9ef7e2
Justin <odeajustin@gmail.com>
2024-04-08 16:38:55 -0600

    added ratings section and overlay for rating.  next steps is adding functionailty.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+// Import necessary modules from react and components
+import React, { useState, useEffect } from "react";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddRatings from "./components/AddRatings";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
+// Define the main App component
 const App = () => {
+  // Define state variables for movies, ratings, and search value
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [ratings, setRatings] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b85bac0`
-
+  // Define a function to fetch movies based on search value
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b85bac0`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
+    // If there are search results, update the movies state
     if (responseJson.Search) {
-    setMovies(responseJson.Search);
-   }
-};
+      setMovies(responseJson.Search);
+    }
+  };
 
-useEffect(()=>{
+  // Use an effect to fetch movies whenever the search value changes
+  useEffect(() => {
     getMovieRequest(searchValue);
+  }, [searchValue]);
 
-}, [searchValue]);
+  // Define a function to add a movie to the ratings list
+  const addRatingMovie = (movie) => {
+    const newRatingsList = [...ratings, movie];
+    setRatings(newRatingsList);
+  };
 
+  // Render the app
   return (
-    <div className='container-fluid movie-reviews'>
-        <div className='row d-flex align-items-center mt-4 mb-4'>
-            <MovieListHeading heading='movies'/>
-            <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+    <div className="container-fluid movie-reviews">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={movies}
+          handleRatingsClick={addRatingMovie}
+          ratingComponent={AddRatings}
+        />
+      </div>
+      <div className="container-fluid movie-reviews">
+        <div className="row d-flex align-items-center mt-4 mb-4">
+          <MovieListHeading heading="Movies I have Rated" />
         </div>
-        <div className='row'>
-        <MovieList movies={movies} />
+        <div className="row">
+          <MovieList
+            movies={ratings}
+            handleRatingsClick={addRatingMovie}
+            ratingComponent={AddRatings}
+          />
         </div>
+      </div>
     </div>
   );
 };
 
+// Export the App component
 export default App;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,24 +1,39 @@
 import React from "react";
+import AddRatings from "./AddRatings";
+import "../App.css";
 
 const MovieList = (props) => {
-    return (
-      <div className='container-fluid movie-reviews'>
-        <div className='row'>
-          {props.movies.map((movie, index) => (
-            <div key={index} className='col-md-4 col-lg-3 col-sm-6 mb-4'>
-              <div className="card">
-                <img src={movie.Poster} className="card-img-top" alt={movie.Title} />
-                <div className="card-body">
-                  <h5 className="card-title">{movie.Title}</h5>
-                  <p className="card-text">{movie.Year}</p>
-                  <p className="card-rating">{movie.Rating}</p>
-                </div>
+  return (
+    <div className="container-fluid movie-reviews">
+      <div className="row">
+        {props.movies.map((movie, index) => (
+          <div key={index} className="col-md-4 col-lg-3 col-sm-6 mb-4">
+            <div className="card h-100">
+              <div className="image-container">
+                {" "}
+                {/* This div wraps the image and overlay */}
+                <img
+                  src={movie.Poster}
+                  className="card-img-top img-fluid"
+                  alt={`Poster of ${movie.Title}`}
+                />
+              </div>
+              <div
+                className="card-body"
+                onClick={() => props.handleRatingsClick(movie)}
+              >
+                <h5 className="card-title">{movie.Title}</h5>
+                <p className="card-text">{movie.Year}</p>
+              </div>
+              <div className="overlay">
+                <AddRatings />
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default MovieList;
